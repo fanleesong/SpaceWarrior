bb47bfc7a8453d0bd335dc2f216ef9fb344ad569
cesc <user@localhost.(none)>
2013-02-27 20:06:32 +0800

    Initial Commit

---
+++ SpaceWarrior/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  SpaceWarriorAppController.h
+//  SpaceWarrior
+//
+//  Created by cesc on 13-2-27.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
